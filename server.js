143b2115267717e2012ab7a72ec3a819dc916dc1
Pool-DEV <40335015+Pool-DEV@users.noreply.github.com>
2024-05-26 22:46:26 +0200

    v1.0
    Développement du hero + navbar (100% responsive)

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer();
+
+const host = "localhost";
+const port = 8080;
+
+server.on("request", (req, res) => {
+    if (req.url.startsWith("/public/")) {
+        try {
+            res.end(fs.readFileSync("." + req.url));
+        } catch (e) {
+            res.writeHead(404);
+            // Prévoir une page d'erreur 404
+            res.end("Erreur 404 : Page non trouvée");
+        }
+    } else if (req.url == "/") {
+        res.end(fs.readFileSync("index.html"));
+    } else {
+        res.writeHead(404);
+        // Prévoir une page d'erreur 404
+        res.end("Erreur 404 : Page non trouvée");
+    }
+});
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
